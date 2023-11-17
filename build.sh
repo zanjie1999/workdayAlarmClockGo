759d4e10d83e1df4078785aeb6a6796f6a2a2b7f
zanjie1999 <zanjie1999@yeah.net>
2023-11-17 14:01:22 +0800

    build

--- build.sh
+++ build.sh
@@ -4,11 +4,17 @@ cd `dirname $0`
 mkdir -p build
 rm -rf build/*
 # export CGO_ENABLED=1
-# export GOARCH=amd64
-# export GOOS=windows
-# go build -ldflags="-w -s"
-# mv workdayAlarmClock.exe build/workdayAlarmClock.exe
+export GOARCH=amd64
+export GOOS=windows
+go build -ldflags="-w -s"
+mv workdayAlarmClock.exe build/workdayAlarmClock.exe
+export GOARCH=386
+go build -ldflags="-w -s"
+mv workdayAlarmClock.exe build/workdayAlarmClock-i386.exe
 export GOOS=linux
+go build -ldflags="-w -s"
+mv workdayAlarmClock build/workdayAlarmClock-linux-i386
+export GOARCH=amd64
 go build -ldflags="-w -s"
 mv workdayAlarmClock build/workdayAlarmClock-linux
 export GOARCH=arm
@@ -16,9 +22,9 @@ go build -ldflags="-w -s"
 mkdir -p $AndroidProject/app/libs/armeabi
 cp workdayAlarmClock $AndroidProject/app/libs/armeabi/libWorkdayAlarmClock.so
 mv workdayAlarmClock build/workdayAlarmClock-linux-arm
-# export GOARCH=mips
-# go build -ldflags="-w -s"
-# mv workdayAlarmClock build/workdayAlarmClock-linux-mips
+export GOARCH=mips
+go build -ldflags="-w -s"
+mv workdayAlarmClock build/workdayAlarmClock-linux-mips
 export GOARCH=arm64
 go build -ldflags="-w -s"
 mkdir -p $AndroidProject/app/libs/arm64-v8a
@@ -27,6 +33,6 @@ mv workdayAlarmClock build/workdayAlarmClock-linux-arm64
 export GOOS=darwin
 go build -ldflags="-w -s"
 mv workdayAlarmClock build/workdayAlarmClock-darwin-arm64
-# export GOARCH=amd64
-# go build -ldflags="-w -s"
-# mv workdayAlarmClock build/workdayAlarmClock-darwin
+export GOARCH=amd64
+go build -ldflags="-w -s"
+mv workdayAlarmClock build/workdayAlarmClock-darwin
