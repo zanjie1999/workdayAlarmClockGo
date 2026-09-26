de4ea9412358e2cf5877c8b4c43117651b4b93a8
zanjie1999 <zanjie1999@yeah.net>
2026-09-26 14:33:09 +0800

    提高作为电脑音箱时的稳定性

--- player/stream_linux.go
+++ player/stream_linux.go
@@ -15,7 +15,7 @@ func playPCMStream(r io.Reader, rate, channels int) error {
 	}
 	cmd := exec.Command(ShellPlayer, "-q", "-t", "raw", "-f", "S16_LE",
 		"-c", strconv.Itoa(channels), "-r", strconv.Itoa(rate),
-		"-B", "30000", "-F", "10000")
+		"-B", "80000", "-F", "20000")
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		return err
